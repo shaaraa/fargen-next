e60dd4fd021b4d0521bd6f5ecd42893f95cfc5c3
shaaraa <shara.sargsyans@gmail.com>
2024-08-01 21:50:06 +0400

    main changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
+import Link from 'next/link'
+import styles from './page.module.css'
+import Image from 'next/image'
 
-const vercelURL = "https://f281-141-136-91-106.ngrok-free.app";
+const vercelURL = process.env.NEXT_PUBLIC_SITE_URL as string;
 const encodedVercelURL = encodeURIComponent(vercelURL);
 const messageText = "Excited for the launch of FarGen! 🎨\n\nGenerate AI art and images in frame\n\nLaunching on August 5 at 14:00 EST 🚀🖼️\n\n👨‍🎨 Frame by: @sharas.eth\n\nShare this frame for a chance to win one of 10 exclusive Early-Pass NFTs to use FarGen frame!👇\n";
 const encodedMessage = encodeURIComponent(messageText);
@@ -20,20 +23,49 @@ export const metadata: Metadata = {
     'fc:frame:image': `${process.env.NEXT_PUBLIC_SITE_URL}/init.gif`,
     'fc:frame:post_url': `${process.env.NEXT_PUBLIC_SITE_URL}/api/check`,
     'fc:frame:button:1': 'Start',
-    'fc:frame:button:2': 'Get early pass',
+    'fc:frame:button:2': 'Share',
     'fc:frame:button:2:action': 'link',
-    'fc:frame:button:2:target': 'https://zora.co/collect/base:0x6ec3b83091a440b46844beabd141b887fd034390/1?referrer=0xdF2D9E58227CE5e37ED3e40BC49d4442C970A2D6',
-    'fc:frame:button:3': 'Share',
+    'fc:frame:button:2:target': `${href}`,
+    'fc:frame:button:3': 'Tip @Sharas.eth',
     'fc:frame:button:3:action': 'link',
-    'fc:frame:button:3:target': `${href}`,
+    'fc:frame:button:3:target': 'https://warpcast.com/sharas.eth/0x54d14dba',
+    'fc:frame:button:4': 'Nominate @Sharas.eth',
+    'fc:frame:button:4:action': 'link',
+    'fc:frame:button:4:target': 'https://build.top/nominate/0xdF2D9E58227CE5e37ED3e40BC49d4442C970A2D6',
+  
   },
 }
 
 export default function Page() {
   return (
-    <div>
-      <h1>FarGen - AI Image Generator</h1>
-      <p>Use this frame to generate AI images directly in Farcaster!</p>
-    </div>
+    <main className={styles.main}>
+      <div className={styles.overlay}>
+        <a 
+          href="https://warpcast.com/sharas.eth" 
+          className={styles.overlayText}
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          Frame by @sharas.eth
+        </a>
+      </div>
+      <div className={styles.container}>
+        <Image
+          src="/initial.gif"
+          alt="Fargen Studio"
+          width={600}
+          height={600}
+          priority
+        />
+        <div className={styles.buttonContainer}>
+          <Link href={`https://warpcast.com/~/compose?text=🎨✨ Generate amazing ai art in-frame with FarGen! 🖼️%0A%0A👨‍🎨 Frame by: @sharas.eth%0A%0A🚀 Try it yourself! 👇%0A&embeds[]=${encodedVercelURL}`} passHref>
+            <button type="button" className={styles.button}>Share on Farcaster</button>
+          </Link>
+          <Link href="https://zora.co/collect/base:0x6ec3b83091a440b46844beabd141b887fd034390/1?referrer=0xdF2D9E58227CE5e37ED3e40BC49d4442C970A2D6" passHref>
+            <button type="button" className={styles.button}>Get Early Pass</button>
+          </Link>
+        </div>
+      </div>
+    </main>
   )
 }
